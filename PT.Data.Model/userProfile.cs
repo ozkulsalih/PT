6926a3f3c8d16256621f3a871e34d61b23378edb
salih.ozkul <salih.ozkul@loti.com.tr>
2024-06-06 20:48:07 +0300

    some commit

---
+++ PT.Data.Model/userProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT.Data.Model
+{
+	public class userProfile
+	{
+		public int id { get; set; }
+		//public user user { get; set; }
+		public int userid { get; set; }
+		public string email { get; set; }
+		public string mobilephone { get; set; }
+		public bool isEmailVerified { get; set; } = false;
+		public bool isMobilephoneVerified { get; set; } = false;
+		public DateTime emailVerifiedOn { get; set; } 
+		public DateTime mobilePhoneVerifiedOn { get; set; }
+		public DateTime createdOn { get; set; } = DateTime.UtcNow;
+		public DateTime modifiedOn { get; set; } = DateTime.UtcNow;
+	}
+}
